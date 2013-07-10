028b52724635f4e63be216f96a6aac89ca3f818a
Yuriy Kaygorodov <gaiverrr@gmail.com>
2013-07-10 14:36:53 +0000

    ttt

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var content;
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ fs.readFileSync('index.html', function(err, data) 
+  {
+       content = data;
+  });
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
