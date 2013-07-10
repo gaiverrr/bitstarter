7e72d3e1c7ce55a655a6a9320623e55956fcf72d
Yuriy Kaygorodov <gaiverrr@gmail.com>
2013-07-10 19:49:10 +0000

    filereader4

--- web.js
+++ web.js
@@ -2,6 +2,21 @@ var express = require('express');
 var fs = require('fs')
 var app = express.createServer(express.logger());
 var content;
+app.get('/', {encoding:'utf8'}, function(request, response) {
+	var result = fs.readFileSync('index.html', function(err, data)
+	{
+		content = data.toString();
+	});
+	response.send(result);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});var express = require('express');
+var fs = require('fs')
+var app = express.createServer(express.logger());
+var content;
 app.get('/', function(request, response) {
  fs.readFileSync('index.html', 'utf8', function(err, data) 
   {
